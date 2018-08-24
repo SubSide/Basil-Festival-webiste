15aaa1d5a3b232281d4777dbf7d946961be8097d
Thomas 'SubSide' van den Bulk <Thomasvdbulk@gmail.com>
2018-08-24 20:45:46 +0200

    Added: Enter on admin list

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -3,5 +3,5 @@
 <a href="{{ route('list.lezingen') }}">Lezingen</a><br />
 <a href="{{ route('list.catering') }}">Catering</a><br />
 <a href="{{ route('list.faqs') }}">Faqs</a><br />
-<a href="{{ route('list.sponsors') }}">Sponsors</a>
+<a href="{{ route('list.sponsors') }}">Sponsors</a><br />
 <a href="{{ route('list.popup') }}">Popupmarkten</a>
